02111fda3a98cd1d547ecb2231e4b7e07777df7c
Umer Saqib <umersaqib160@hotmail.com>
2016-05-24 18:19:02 +0200

    to be continued

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,21 @@
+
+
 //= require jquery
 //= require bootstrap-sprockets
-//= require_tree .
+//= require_tree
+
+$(document).ready(function() {
+
+  $(window).scroll(function () {
+    //if you hard code, then use console
+    //.log to determine when you want the
+    //nav bar to stick.
+    console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 100) {
+      $('.navbar').addClass('navbar-scroll');
+    }
+    if ($(window).scrollTop() < 101) {
+      $('.navbar').removeClass('navbar-scroll');
+    }
+  });
+});
